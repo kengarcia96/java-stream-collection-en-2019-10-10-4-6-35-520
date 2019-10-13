b0a648c6d2e6aacf1703c1e8c77fac42f09ded6b
John Kenneth Garcia <kengarcia.96@gmail.com>
2019-10-13 13:36:57 +0800

    Answers for Collections should_return_list_by_Interval and should_return_list_by__two_Intervals

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,15 +2,45 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+
+        List<Integer> finalList = new ArrayList<>();
+        int smallerNumber = Math.min(left, right);
+        int biggerNumber = Math.max(left,right);
+
+        for (int x=smallerNumber; x<=biggerNumber; x++){
+                finalList.add(x);
+        }
+
+        if (left >= right){
+            finalList.sort(Comparator.reverseOrder());
+        }
+
+        return finalList;
+
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
-        throw new NotImplementedException();
+        List<Integer> finalList = new ArrayList<>();
+        int smallerNumber = Math.min(left, right);
+        int biggerNumber = Math.max(left,right);
+
+        for (int x=smallerNumber; x<=biggerNumber; x++){
+            if (x % 2 == 0) {
+                finalList.add(x);
+            }
+        }
+
+        if (left >= right){
+            finalList.sort(Comparator.reverseOrder());
+        }
+
+        return finalList;
     }
 
     public List<Integer> popEvenElments(int[] array) {
